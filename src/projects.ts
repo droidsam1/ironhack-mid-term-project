44a4df7ca8c88af88eb655742e6edeca18cbc800
Samuel Taboas <droidsam@gmail.com>
2022-07-19 19:12:13 +0200

    add test for calling posts api

--- src/projects.ts
+++ src/projects.ts
@@ -1,2 +1,26 @@
-let message: string = 'Hello, (typed 🤓) World!';
-console.log(message);
+const BASE_URL =
+  "https://mockend.com/droidsam1/ironhack-mid-term-project-mockend/posts";
+
+const GRAPHQL_BASE_URL =
+  "https://mockend.com/droidsam1/ironhack-mid-term-project-mockend/graphql?";
+
+export default async function getThreePost() {
+  const response = await fetch(GRAPHQL_BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `{
+            posts(limit: 3) {
+              id,
+              title,
+              body,
+              cover
+            }
+          }`,
+    }),
+  });
+  const json = await response.json();
+  return json.data.posts;
+}
