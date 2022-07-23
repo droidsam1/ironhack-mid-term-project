c718fcb301ec7eb0b2ef61c3acf777f263ce0c3e
Samuel Taboas <droidsam@gmail.com>
2022-07-23 15:13:59 +0200

    add subscription functionality

---
+++ src/subscription.ts
@@ -0,0 +1,112 @@
+export async function subscribe(event?: { preventDefault: () => void }) {
+  event?.preventDefault();
+
+  if (!validateForm()) {
+    return;
+  }
+
+  showPleaseWaitMessage();
+
+  const userMail = getUserMail();
+  await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      mail: userMail,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(async (response) => {
+      if (response.status === 200) {
+        hideButton();
+        hideInput();
+        showSubmissionMessage();
+      } else {
+        await showError();
+      }
+    })
+    .catch(async () => {
+      await showError();
+    })
+    .finally(() => {
+      restoreSubscribeButton();
+    });
+}
+
+function validateForm() {
+  const form = document.querySelector(".cta-section__mail") as HTMLFormElement;
+  form.checkValidity();
+  form.reportValidity();
+  return form.checkValidity();
+}
+
+function getUserMail(): String {
+  const subscribeButton = document.querySelector(
+    ".cta-section__mail-input"
+  ) as HTMLInputElement;
+  return subscribeButton.value;
+}
+
+function showPleaseWaitMessage() {
+  const subscribeButton = document.querySelector(
+    ".cta-section__subscribe-button"
+  ) as HTMLInputElement;
+
+  subscribeButton.innerHTML = "Please wait...";
+}
+
+function showSubmissionMessage() {
+  const submissionMessage = document.querySelector(
+    ".submitted-message"
+  ) as HTMLElement;
+
+  submissionMessage.style.display = "block";
+  submissionMessage.style.visibility = "visible";
+}
+
+function hideButton() {
+  const subscribeButton = document.querySelector(
+    ".cta-section__subscribe-button"
+  ) as HTMLInputElement;
+
+  subscribeButton.style.display = "none";
+  subscribeButton.style.visibility = "hidden";
+}
+
+function hideInput() {
+  const subscribeInput = document.querySelector(
+    ".cta-section__mail-input"
+  ) as HTMLElement;
+
+  subscribeInput.style.display = "none";
+  subscribeInput.style.visibility = "hidden";
+}
+
+function restoreSubscribeButton() {
+  const subscribeButton = document.querySelector(
+    ".cta-section__subscribe-button"
+  ) as HTMLInputElement;
+  subscribeButton.style.backgroundColor = "var(--main-blue-color)";
+  subscribeButton.innerHTML = "Subscribe";
+}
+
+function showError() {
+  const subscribeButton = document.querySelector(
+    ".cta-section__subscribe-button"
+  ) as HTMLInputElement;
+
+  subscribeButton.innerHTML = "Something went wrong!";
+  subscribeButton.style.backgroundColor = "red";
+  return new Promise((res) =>
+    setTimeout(() => res("Enough time to see the error"), 4000)
+  );
+}
+
+window.addEventListener("load", () => {
+  const subscribeButton = document.querySelector(
+    ".cta-section__subscribe-button"
+  ) as HTMLInputElement;
+
+  subscribeButton.addEventListener("click", subscribe);
+});
---
+++ tests/subscription.test.ts
@@ -0,0 +1,94 @@
+import { subscribe } from "../src/subscription";
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    status: 200,
+    json: () =>
+      Promise.resolve({
+        data: {},
+      }),
+  })
+) as jest.Mock;
+// Base document body
+document.body.innerHTML = `<section class="cta-section">
+<div>
+    <h3 class="h3-heading black">Do you have any questions?</h3>
+</div>
+<div class="text-regular main-grey">Let us help you!</div>
+<form class="cta-section__mail">
+    <input class="cta-section__mail-input" type="email" placeholder="Enter your email" required />
+    <button class="rectangular-main-button cta-section__subscribe-button" type="submit">Subscribe</button>
+    <div class="submitted-message">
+    Thank you! Your submission has been received!
+</div>
+</form>
+</section>`;
+
+describe("Subscription", () => {
+  beforeAll(() => {
+    const subscribeButton = document.querySelector(
+      ".cta-section__mail button"
+    ) as HTMLElement;
+    subscribeButton.addEventListener("click", subscribe);
+
+    const subscribeInput = document.querySelector(
+      ".cta-section__mail-input"
+    ) as HTMLInputElement;
+    subscribeInput.value = "something@mail.com";
+  });
+
+  it("should restore the button message when post fail", async () => {
+    (fetch as jest.Mock).mockResolvedValueOnce({
+      status: 400,
+    });
+
+    await subscribe();
+
+    const subscribeButton = document.querySelector(
+      ".cta-section__subscribe-button"
+    ) as HTMLElement;
+
+    expect(subscribeButton.innerHTML).toEqual("Subscribe");
+  });
+
+  it("should hide the button when submission succeed", async () => {
+    const subscribeButton = document.querySelector(
+      ".cta-section__subscribe-button"
+    ) as HTMLElement;
+
+    await subscribe();
+
+    expect(subscribeButton.style).toHaveProperty("visibility", "hidden");
+    expect(subscribeButton.style).toHaveProperty("display", "none");
+  });
+
+  it("should hide the input when submission succeed", async () => {
+    const subscribeButton = document.querySelector(
+      ".cta-section__subscribe-button"
+    ) as HTMLElement;
+
+    const subscribeInput = document.querySelector(
+      ".cta-section__mail-input"
+    ) as HTMLElement;
+
+    await subscribe();
+
+    expect(subscribeInput.style).toHaveProperty("visibility", "hidden");
+    expect(subscribeInput.style).toHaveProperty("display", "none");
+  });
+
+  it("should show the submittted message when submission succeed", async () => {
+    const subscribeButton = document.querySelector(
+      ".cta-section__subscribe-button"
+    ) as HTMLElement;
+
+    const submissionMessage = document.querySelector(
+      ".submitted-message"
+    ) as HTMLElement;
+
+    await subscribe();
+
+    expect(submissionMessage.style).toHaveProperty("display", "block");
+    expect(submissionMessage.style).toHaveProperty("visibility", "visible");
+  });
+});
