7ee846fbb15a976509e929df6d2feed2a0416372
droidsam1 <droidsam@gmail.com>
2022-07-27 18:45:42 +0200

    improve navbar responsiveness

--- src/burguer-menu.ts
+++ src/burguer-menu.ts
@@ -1,10 +1,13 @@
 function toggleBurguer() {
-    var menu = document.querySelector("#navbar__menu");
+    let menu = document.querySelector("#navbar__menu") as HTMLElement;
+    let navbar = document.querySelector(".navbar") as HTMLElement;
     if (menu.classList.contains('navbar__menu__hidden')) {
-        menu.classList.remove('navbar__menu__hidden')
+        menu.classList.remove('navbar__menu__hidden');
+        navbar.style.marginBottom = '8rem';
     }
     else {
         menu.classList.add('navbar__menu__hidden');
+        navbar.style.marginBottom = '0';
     }
 }
 window.addEventListener("load", function () {
